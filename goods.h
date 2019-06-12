04bee38ed4501bfe716b6d4feca54aadd7c1983e
irproximity <51580162+irproximity@users.noreply.github.com>
2019-06-12 16:23:30 +0900

    Update goods.h

--- goods.h
+++ goods.h
@@ -1,11 +1,6 @@
 #ifndef GOODS_H_
 #define GOODS_H_
 
-// Goods class for representing goods information with index and price
-// Example:
-//    Goods test;
-//    test.SetGoodsInfo(1, 10000);
-//    std::cout << test.GetIndex() << " - " << test.GetPrice << endl;
 class Goods{
  public:
   void SetGoodsInfo(int index, int price);
