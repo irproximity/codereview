cfde1855087b1a8bc67606a407cfa3a6cef67a91
irproximity <51580162+irproximity@users.noreply.github.com>
2019-06-12 15:21:19 +0900

    Update shopping.h

--- shopping.h
+++ shopping.h
@@ -1,18 +1,10 @@
 #ifndef SHOPPING_H_
 #define SHOPPING_H_
 
-#include <vector>
 #include "./goods.h"
 
-// Shopping class presents List of shopping.
-// Example:
-//     Shopping tobuylist;
-//     Goods obj1;
-//     Goods obj2;
-//     ...
-//     tobuylist.PushList(obj1);
-//     tobuylist.PushList(obj2);
-//     tobuylist.PrintList();
+#include <vector>
+
 class Shopping{
  public:
   void PushList(Goods object);
